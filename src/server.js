4513fafbf701178ee51975df127733b8129a1c49
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-06-29 13:58:46 +0300

    inslall pino

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.get("/notes", (req, res) => {
 
 app.get("/notes/:noteId", (req, res) => {
   const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` }); 
 });
 
 app.get("/test-error", () => {
