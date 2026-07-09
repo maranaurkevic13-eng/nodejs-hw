d801ec58cb65fd3e7561144b1bf1c2f5ff47fac7
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-07-09 13:51:41 +0300

    fix: validation schemas

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { getAllNotes, getNoteById, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
+import { getAllNotesSchema, noteIdSchema, createNoteSchema, updateNoteSchema } from "../validations/notesValidation.js";
+import { celebrate } from 'celebrate';
 
 const router = Router();
 
-router.get("/notes", getAllNotes);
-router.get("/notes/:noteId", getNoteById);
-router.post("/notes", createNote);
-router.patch("/notes/:noteId", updateNote);
-router.delete("/notes/:noteId", deleteNote);
+router.get("/notes", celebrate(getAllNotesSchema), getAllNotes);
+router.get("/notes/:noteId", celebrate(noteIdSchema), getNoteById);
+router.post("/notes", celebrate(createNoteSchema), createNote);
+router.patch("/notes/:noteId", celebrate(updateNoteSchema), updateNote);
+router.delete("/notes/:noteId", celebrate(noteIdSchema), deleteNote);
 
 export default router;
