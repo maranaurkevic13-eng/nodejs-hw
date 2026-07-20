ba4aa5b9e7bef193ed50934b0b6131bfc5f5f7e0
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-07-20 10:43:56 +0300

    change server file

--- src/server.js
+++ src/server.js
@@ -7,15 +7,20 @@ import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
+app.use(cookieParser());
 
 await connectMongoDB();
 
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
